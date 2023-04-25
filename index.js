0efe73beec859b55bc258358b88d1c40922fed0b
GRLPWR1 <110125337+GRLPWR1@users.noreply.github.com>
2023-04-25 20:36:53 +0300

    optimised the code

--- index.js
+++ index.js
@@ -2,17 +2,9 @@
 // и выводит true, если строки являются анаграммами, и false, если строки ими не являются.
 
 function isAnagrams(str1, str2){
-    let arr1 = str1.split('');
-    let arr2 = str2.split('');
-    let sameCharacter = 0;
-    for (let i = 0; i < arr1.length; i++){
-        for (let j = 0; j < arr2.length; j++){
-            if(arr1[i] === arr2[j]) {
-                sameCharacter += 1;
-            }
-        }
-    }
-    if (sameCharacter == arr1.length && sameCharacter == arr2.length) {
+    let str1Sorted = str1.split('').sort().join('');
+    let str2Sorted = str2.split('').sort().join('');
+    if (str1Sorted === str2Sorted) {
         return true;
     } else {
         return false;
